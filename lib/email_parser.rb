9c3cbcc2daacb1f59f4c8a5a3f624a4d8d72741a
Shaina C <scailles23@gmail.com>
2020-04-22 23:36:58 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,19 +5,18 @@
 
 
 class EmailParser
-  
-  attr_accessor :emails
-  
-  
-  def initialize(emails)
-    @emails = emails
+
+  attr_accessor :email
+  def  initialize(emails)
+    @email = emails
   end
-  
-   def parse
+
+  def parse
     email_array = @email.split(/[, ]/).uniq
     email_array.reject! {|element| element.empty?}
     email_array
   end
 
+end
   
 end
